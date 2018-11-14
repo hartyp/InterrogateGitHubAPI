7c753808fb3e8e74c51aae8f7ff39f1bad7da156
hartyp <hartyp@tcd.ie>
2018-11-14 17:46:24 +0000

    Created a URL connection for the repository

---
+++ GitHub API/src/gitHubAPI.java
@@ -0,0 +1,21 @@
+//Programme returns the number of downloads from 
+//a given github repository and given user name.
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+public class gitHubAPI {
+	public static void main(String[] args) throws Exception {
+
+		String userName = "hartyp";
+		String repoName = "CS3012AssignmentOne";
+		HttpURLConnection connection = (HttpURLConnection) new URL(
+				"https://api.github.com/repos/" + userName + "/" + repoName + "/releases").openConnection();
+		BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+	}
+
+}
